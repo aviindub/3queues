3d8c79eaaa6ef26d0062a8f4755d154686ff0112
avi goldberg <aviindub@gmail.com>
2012-09-22 21:19:17 -0400

    added unit tests for each public function

--- test/Queue.cpp
+++ test/Queue.cpp
@@ -1,23 +1,32 @@
 #include "gtest/gtest.h"
 #include "../src/Queue.h"
 
-TEST(QueueTest, Enqueue) {
-  Queue* s = new Queue();
-  s->enqueue(6);
+TEST(QueueTest, EnqueueDequeue) {
+    Queue* q = new Queue();
+    q->enqueue(6);
 
-  EXPECT_EQ(6, s->dequeue());
-  delete s;
+    EXPECT_EQ(6, q->dequeue());
+    delete q;
 }
 
+TEST(QueueTest, Size) {
+
+    Queue* q = new Queue();
+    for (int i = 0; i < 5; i++) {
+        q->enqueue(i);
+    }
+    
+    EXPECT_EQ(5, q->size());
+}
 
 TEST(QueueTest, Resize) {
-    Queue* s = new Queue();
+    Queue* q = new Queue();
     for (int i = 0; i <= 24; i++) {
-        s->enqueue(i);
+        q->enqueue(i);
     }
-    EXPECT_EQ(24, s->size());
-    EXPECT_EQ(0, s->dequeue());
-    EXPECT_EQ(1, s->dequeue());
-    EXPECT_EQ(2, s->dequeue());
-    delete s;
+    EXPECT_EQ(24, q->size());
+    EXPECT_EQ(0, q->dequeue());
+    EXPECT_EQ(1, q->dequeue());
+    EXPECT_EQ(2, q->dequeue());
+    delete q;
 }
